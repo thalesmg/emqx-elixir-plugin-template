ca54b55580d06f8370c8b6628c0f32f69b9a0bc9
Thales Macedo Garitezi <thalesmg@gmail.com>
2022-02-22 15:48:00 -0300

    empty

--- test/elixir_plugin_template_test.exs
+++
@@ -1,9 +0,0 @@
-defmodule ElixirPluginTemplateTest do
-  use ExUnit.Case
-  doctest ElixirPluginTemplate
-
-  test "pings" do
-    assert ElixirPluginTemplate.ping() == 1
-    assert ElixirPluginTemplate.ping() == 2
-  end
-end
